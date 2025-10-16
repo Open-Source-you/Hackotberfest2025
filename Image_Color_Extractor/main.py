99e974470896d09b0d9736627db3af5483e08a2b
MadhankumarAI <jaip90080@gmail.com>
2025-10-16 13:23:51 +0530

    Added Image Color Extractor using KMeans Clustering (Hacktoberfest 2025) – resolves PEP8 checks

--- Image_Color_Extractor/main.py
+++ Image_Color_Extractor/main.py
@@ -1,5 +1,5 @@
 """
-Image Color extractor using KMeans Clustering
+Image Color Extractor using KMeans Clustering
 
 Author: Madhan Kumar R
 Date: 16 October 2025
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from sklearn.cluster import KMeans
 
-# Place your image file in the same directory as this script
-# and name it 'test.jpg' or change the filename below accordingly.
+# Place your image file in the same
+# directory as this script and name it 'test.jpg'.
 img = mpimg.imread('test.jpg')
 print("Image exists")
 
@@ -19,12 +19,22 @@ image = mpimg.imread('test.jpg')
 w, h, d = image.shape
 pixels = image.reshape((w * h, d))
 print("Image reshaped")
-n_colors = 10
-kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(pixels)
+
+n_colors = int(input("Enter number of colors to extract: "))
+kmeans = KMeans(
+    n_clusters=n_colors,
+    random_state=0,
+    n_init="auto"
+).fit(pixels)
+
 print("KMeans fitted")
+
 palette = np.uint8(kmeans.cluster_centers_)
 
 plt.imshow([palette])
 plt.axis('off')
 plt.show()
 print("Palette displayed")
+print("Palette colors (RGB):")
+for color in palette:
+    print(color)
